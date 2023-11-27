1628b9be02a8b21dc35c2cb4738c659c23d75c69
Andrea  Pavesich <andrealynn@AndreasMacBookAir2.local>
2023-11-27 16:17:43 -0600

    adding cart button

--- src/cart.php
+++ src/cart.php
@@ -157,7 +157,7 @@ $total_price += ($product["toy_price"]*$product["toy_quantity"]);
 <?php echo $status; ?>
 </div>
 
-
+<button style="align:center" class="btn btn-success">Proceed to Payment</button>
 
  <!-- Javascript import with popper for dropdowns-->
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
